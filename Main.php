7f100e1b20fa47bd24dda0a5e8ed5f8937c46694
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2021-12-01 18:41:25 +0300

    env support

--- Main.php
+++ Main.php
@@ -1,15 +1,17 @@
 <?php
+include_once 'vendor/autoload.php';
 
 class Main
 {
-    const TOKEN = '2137836088:AAE7pMqCqMnJmKZIRfbMuhYOUhUsSyldPPU';
-
     private $apiUrl;
     private $db;
 
     public function __construct()
     {
-        $this->apiUrl = "https://api.telegram.org/bot" . $this::TOKEN . "/";
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+        $this->apiUrl = "https://api.telegram.org/bot" . $_ENV['TG_TOKEN'] . "/";
         $this->db = new mysqli("localhost", "root", "", "tgbot_db");
     }
 
@@ -22,6 +24,9 @@ class Main
             $resp = $this->makeRequest("getUpdates?offset={$offset}");
             // Получаю массив Update
             foreach ($resp->result as $update) {
+                if (!isset($update->message)) {
+                    continue;
+                }
                 $chatId = $update->message->chat->id;
                 // Определяю имя
                 $name = isset($update->message->chat->first_name) ? $update->message->chat->first_name : 'none';
@@ -45,11 +50,14 @@ class Main
                     if ($isBotCommand) {
                         switch ($update->message->text) {
                             case '/start':
-                                $messageText = urlencode('Бот запущен! ' . $name);
+                                $messageText = urlencode("{$name}, бот запущен!");
                                 $this->makeRequest("sendMessage?chat_id={$chatId}&text={$messageText}");
                                 break;
                         }
                     }
+                }
+                if (isset($update->message->text) && $update->message->text == 'Пидар') {
+                    $this->makeRequest("pinChatMessage?chat_id={$chatId}&message_id={$update->message->message_id}");
                 }
                 $updateId = $update->update_id;
             }
